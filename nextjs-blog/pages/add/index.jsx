103abb3d7b04e06a26c882583328e8f0870872cc
hanguyen55 <58657556+hanguyen12345@users.noreply.github.com>
2021-07-21 15:39:21 +0700

    first commit

---
+++ nextjs-blog/pages/add/index.jsx
@@ -0,0 +1,8 @@
+import Add from '../../components/add/add';
+
+export default function Home() {
+    return (
+        <>
+            <Add />
+        </>
+    )}
---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,8 @@
+import Add from '../pages/add';
+
+export default function Home() {
+    return (
+        <>
+            <Add />
+        </>
+    )}
